b581f9078b9654e138add075369a2ca7c11a4660
okkyprasetian <okkyprasetian@gmail.com>
2021-11-30 07:40:44 +0700

    complete input and submit functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { useState, useEffect } from 'react'
+import data from './data'
 
 function App() {
+
+  const [fluidNum, setFluidNum] = useState(0)
+  const [fixNum, setFixNum] = useState(0)
+  const [text, setText] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setFixNum(fluidNum)
+    setFluidNum(0)
+  }
+
   return (
     <div className="App">
+      <h1>TIRED OF BORING LOREM IPSUM?</h1>
+      <form className="form" onSubmit={handleSubmit} >
+        <span className="par">Paragraphs: </span>
+        <input type="number" value={fluidNum} onChange={e => { setFluidNum(e.target.value) }} />
+        <input type="submit" value="Submit" />
+      </form>
 
     </div>
   );
