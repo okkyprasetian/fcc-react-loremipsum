e624cd364603ebdda38a5d382915b930acc81cd0
okkyprasetian <okkyprasetian@gmail.com>
2021-11-30 08:35:11 +0700

    complete styling phase 1 : basic layout for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import data from './data'
 
 function App() {
 
   const [fluidNum, setFluidNum] = useState(0)
-  const [fixNum, setFixNum] = useState(0)
   const [text, setText] = useState('')
 
   const handleSubmit = event => {
     event.preventDefault()
-    setFixNum(fluidNum)
 
     if (fluidNum > 0 && fluidNum <= data.length) {
       let newData = [...data].slice(0, fluidNum)
@@ -25,9 +23,9 @@ function App() {
     <div className="App">
       <h1>TIRED OF BORING LOREM IPSUM?</h1>
       <form className="form" onSubmit={handleSubmit} >
-        <span className="par">Paragraphs: </span>
-        <input type="number" max={data.length} value={fluidNum} onChange={e => { setFluidNum(e.target.value) }} />
-        <input type="submit" value="Submit" />
+        <span className="input-par">Paragraphs: </span>
+        <input type="number" className="input-num" max={data.length} value={fluidNum} onChange={e => { setFluidNum(e.target.value) }} />
+        <input type="submit" className="input-btn" value="GENERATE" />
       </form>
 
       {text !== '' &&
